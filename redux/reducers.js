7631d0ed327c2e622c1a489b3044b3c90b8c76bf
mbeerifeldman <114952150+mbeerifeldman@users.noreply.github.com>
2023-09-25 00:07:23 -0700

    Update reducers.js

--- redux/reducers.js
+++ redux/reducers.js
@@ -1,66 +1,71 @@
 import { combineReducers } from "redux"
 
-
 const initialState = {
-    isFavorite: false,
-    favorites: [],
-    lat : 0,
-    lng: 0,
-    city : '',
-}
+  isFavorite: false,
+  favorites: [],
+  city : '',
+  searchBar: '',
+  cityName: '',
+  cityKey: '',
+  current: '',
+  fiveDay: '',
+  //error: false,
+};
 
-const isFavoriteReducer = (state = initialState.isFavorite, action) => {
+function fetchReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_SEARCH_BAR':
+      return { ...state, searchBar: action.payload };
+    case 'SET_CITY_NAME':
+      return { ...state, cityName: action.payload };
+    case 'SET_CITY_KEY':
+      return { ...state, cityKey: action.payload };
+    case 'SET_CURRENT':
+      return { ...state, current: action.payload };
+    case 'SET_FIVE_DAY':
+      return { ...state, fiveDay: action.payload };
+    default:
+      return state;
+  }}
+
+const isFavoriteReducer = (state = initialState, action) => {
     switch (action.type) {
       case 'FAVE_TOGGLE':
-        return action.payload
+        return {isFavorite : action.payload}
+      case 'ERROR_TOGGLE':
+        return {error : action.payload}
       default:
         return state
     }
   }
   
-  const favoritesReducer = (state = initialState.favorites, action) => {
+  const favoritesReducer = (state = initialState, action) => {
     switch (action.type) {
       case 'SET_FAVORITES':
-        return action.payload
-      default:
-        return state
-    }
-  }
-  const latReducer = (state = initialState.lat, action) =>{
-    switch(action.type){
-        case 'SET_LAT':
-            return action.payload
-        default:
-            return state
-    }
-  }
-
-  const lngReducer = (state = initialState.lng, action) =>{
-    switch(action.type){
-        case 'SET_LNG':
-            return action.payload
-        default:
-            return state
-    }
-  }
-
-  const cityReducer = (state = initialState.city, action) =>{
-    switch (action.type) {
+        return { ...state, favorites: action.payload };
       case 'SET_CITY':
-        return action.payload
+        return { ...state, city: action.payload };
       default:
         return state;
+    }
+  }  
+  const coorReducer = (state = 0,  action) =>{
+    switch(action.type){
+        case 'SET_LAT':
+            return { lat : action.payload }
+        case 'SET_LNG':
+            return {lng : action.payload}
+      default:
+        return state
     }}
 
 
-
-
-  
+ 
 export const rootReducer = combineReducers({
     isFavorite: isFavoriteReducer,
     favorites: favoritesReducer,
-    lat : latReducer,
-    lng : lngReducer,
-    city: cityReducer
+    coor: coorReducer,
+    fetch : fetchReducer
   })
+
 
